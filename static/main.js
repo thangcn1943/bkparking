3b39b0e0b94ace8842c077f225889fecec4beb9b
Thang <thang19431@gmail.com>
2024-03-10 14:34:46 +0700

    dua file len

---
+++ static/main.js
@@ -0,0 +1,6 @@
+// pop xóa cuối
+// shift xóa đầu
+// splice cắt splice(vị trí đặt trỏ, xóa bao nhiêu phần tử từ vị trí trỏ)
+// splice(1,1, 'Dart') xóa ở vị trí thứ 1 và thêm Dart vào thay
+// concat ghép mảng
+// slice cắt giống python slice(a,b) a: vị trí đặt trỏ, b: vị trí kết thúc
